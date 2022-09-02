89c065e701e238c02a2a8dca35363569765417e1
Mikhail Vagin <mvagin@bk.ru>
2022-09-03 01:57:57 +0400

    added classnames

--- src/components/templates/TestTemplate/index.tsx
+++ src/components/templates/TestTemplate/index.tsx
@@ -1,6 +1,8 @@
-import { useCounterStoreShallow } from '@/stores/counter';
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
+import cn from 'classnames';
+import { useCounterStoreShallow } from '@/stores/counter';
+import styles from './index.module.scss';
 
 export const TestTemplate = memo(function TestTemplate() {
   const { id } = useParams();
@@ -14,10 +16,10 @@ export const TestTemplate = memo(function TestTemplate() {
     <>
       <h1>Test Template with id params - {id}</h1>
       <br />
-      <h2>{value}</h2>
+      <h2 className={cn(styles.title, value % 2 === 0 ? styles.green : null)}>{value}</h2>
       <br />
-      <button onClick={() => increase(2)}>increase</button>
-      <button onClick={() => decrease()}>decrease</button>
+      <button onClick={() => increase(2)}>increase +2</button>
+      <button onClick={() => decrease()}>decrease -1</button>
     </>
   );
 });
